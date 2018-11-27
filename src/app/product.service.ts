f10eb92e4a8f4a745359df99b10ddc63d847d6ff
Paul Astro <plppp2001@gmail.com>
2018-11-27 18:24:49 +0100

    Completed the module

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -16,7 +16,8 @@ export class ProductService {
 
 
   getAlbum(id: number){
-        return this._http.get(this._albumUrl).map((response) => response.json());
+        return this._http.get(this._albumUrl).map((response) => 
+        response.json());
    }
 
 }
